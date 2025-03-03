328af2e1901ec065cb1e60acd77913a24e9109cb
ortbri <brian.ort02@icloud.com>
2025-03-03 15:03:11 -0600

    fix:header not full screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="min-h-screen w-full px-5 pt-24 pb-5 flex ">
+    <main className="min-h-72 w-full px-5 pt-24 pb-5 flex ">
       <div className="mx-auto w-full max-w-7xl  rounded-3xl  ">
           {/* Hello! */}
       </div>
